caf90b7405a9b386a6b6f781fc54a00f98ff1aec
Gapur <kaloycant@gmail.com>
2021-09-29 18:33:04 +0300

    #5 init

---
+++ app_05/react_app/src/App.js
@@ -0,0 +1,15 @@
+import Comp1 from './Comp1';
+import Comp2 from './Comp1';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Comp1/>
+      <hr />
+      <Comp2/>
+    </div>
+  );
+}
+
+export default App;
