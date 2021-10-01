e526879870959fef0bbd7df34aeab34f7d2e34e2
Gapur <kaloycant@gmail.com>
2021-10-01 20:37:32 +0300

    #5 again

--- app_05/react_app/src/Comp1.js
+++ app_05/react_app/src/Comp1.js
@@ -2,13 +2,11 @@ import React from 'react';
 
 class Comp1 extends React.Component {
 
-    constructor () {
+    constructor() {
         super();
         this.state = {
             count: 0,
-            comment: [],
         }
-        this.myRef = React.createRef();
     }
 
     handler = () => {
@@ -17,20 +15,12 @@ class Comp1 extends React.Component {
         this.setState({count: currentCount});
     }
 
-    addComment = () => {
-        let comment = this.myRef.current.value;
-        let comments = this.state.comment;
-        comments.push(comment);
-        this.setState({"comments": comments});
-        this.myRef.current.value = '';
-    }
-
     render() {
         return (
-            <>
+            <div>
                 <h1>State</h1>
                 <div>
-                    <button onClick={this.handler} >This button changes state</button>
+                    <button onClick={this.handler}>This button change state</button>
                 </div>
                 <div>
                     {this.state.count % 2 === 0 ? 'even' : 'odd'}
@@ -38,20 +28,10 @@ class Comp1 extends React.Component {
                 <div>
                     {this.state.count}
                 </div>
-                <div>
-                    <textarea ref={this.myRef} name="" id="" cols="30" rows="10"></textarea>
-                </div>
-                <div>
-                    <button onClick={this.addComment} className="add-comment">Add comment</button>
-                </div>
-                <div>
-                    <ul>
-                        {this.state.comment.map((item, index) => <li key={index.toString()}>{item}</li>)}
-                    </ul>
-                </div>
-            </>
+            </div>
         );
     }
+
 }
 
 export default Comp1;
